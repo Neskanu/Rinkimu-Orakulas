23c6a16186c3fa5661d775b14e866acd27eabad2
neskanu <vjurgaitis@gmail.com>
2026-04-13 22:10:14 +0300

    feat: initialize SQLAlchemy database layer, add base HTML template, and update gitignore with standard exclusions

--- app/data/database.py
+++ app/data/database.py
@@ -1,3 +1,4 @@
+import contextlib
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
@@ -11,6 +12,7 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+@contextlib.contextmanager
 def get_db():
     db = SessionLocal()
     try:
